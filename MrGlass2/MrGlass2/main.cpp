6af74ed9202fd3a1107fe452b0392f3100f61c06
Monni <h4211@student.jamk.fi>
2015-11-17 19:57:48 +0200

    Menu aloittelua

--- MrGlass2/MrGlass2/main.cpp
+++ MrGlass2/MrGlass2/main.cpp
@@ -1,9 +1,60 @@
+#include <SFML/Graphics.hpp>
 #include <iostream>
-using namespace std;
+#include "Menu.h"
 
-int main(void) {
 
-	cout << "Hello world";
-	return 0;
 
-}
+// SFML 2.1 tutorial 51
+int main() {
+
+	sf::RenderWindow window(sf::VideoMode(1024, 720), "MrGlass2");
+
+
+	Menu menu(window.getSize().x, window.getSize().y);
+
+	while (window.isOpen()) {
+		sf::Event event;
+
+		while (window.pollEvent(event)) {
+			switch (event.type) {
+			case sf::Event::KeyReleased:
+				switch (event.key.code) {
+
+				case sf::Keyboard::Up:
+					menu.MoveUp();
+					break;
+
+				case sf::Keyboard::Down:
+					menu.MoveDown();
+					break;
+
+				case sf::Keyboard::Return:
+					switch (menu.GetPressedItem()) {
+					case 0:
+						std::cout << "Play button has been pressed" << std::endl;
+						break;
+					case 1:
+						std::cout << "Options button has been pressed" << std::endl;
+						break;
+					case 2:
+						window.close();
+						break;
+					}
+
+					break;
+				}
+				break;
+
+			case sf::Event::Closed:
+				window.close();
+				break;
+			}
+		}
+			window.clear();
+
+			menu.draw(window);
+
+			window.display();
+		}
+
+	}
